002aa85a31f03882e62487621879699fdbc678bb
Robin Ricard <ricard.robin@gmail.com>
2016-05-18 19:02:17 +0200

    Add post listing
    as well as some doc

--- src/models/Post.js
+++ src/models/Post.js
@@ -7,7 +7,17 @@ import {
 
 import orm from "../backends/object-relational-mapper"
 
+// # Post model
+//
+// The most basic model of the blog engine. Don't actually refer to the content
+// but defines the main metadata of a post.
+//
+// ## Class
 class Post extends orm.Model {
+  // ### Constructor
+  //
+  // We'll build on the inherited constructor of Bookshelf, all we do is add
+  // some event hooks.
   constructor(...args) {
     super(...args)
 
@@ -21,22 +31,45 @@ class Post extends orm.Model {
     })
   }
 
+  // ### Title field
+  //
+  // - **Returns** The title of the post
   title(): string {
     return this.get("title")
   }
 
+  // ### Creation Date field
+  //
+  // - **Returns** The date of creation of the post
   creationDate(): Date {
     return new Date(this.get("created_at"))
   }
 
+  // ### Update Date field
+  //
+  // - **Returns** The date of last update of the post
   updateDate(): Date {
     return new Date(this.get("updated_at"))
   }
+
+  // ### All posts retriever
+  //
+  // - **Returns** all posts
+  static allPosts(): Promise<Array<Post>> {
+    return new Post()/*.orderBy("-updated_at")*/.fetchAll()
+    .then(coll => coll.models)
+  }
 }
 
+// ### Table setup
+//
+// Let the Post model act on the `posts` table
 Post.prototype.tableName = "posts"
 
-export default GraphQuill.createType(Post, {
+// ## Type Wrapping
+//
+// Annotate GraphQL types around the Post model
+Post = GraphQuill.createType(Post, {
   name: "Post",
   description: "Represents an authored post in the blog",
   idField: "id",
@@ -54,3 +87,11 @@ export default GraphQuill.createType(Post, {
     type: GraphQLString,
   },
 })
+
+// As well as for its static methods that be considered as root queries
+Post.allPosts = GraphQuill.createRootQueryConnection(Post.allPosts, {
+  name: "allPosts",
+  connectedType: () => Post,
+})
+
+export default Post
--- test/models/Post.js
+++ test/models/Post.js
@@ -4,12 +4,26 @@ import assert from "assert"
 
 import Post from "../../src/models/Post"
 
+const WELCOME_POST_TITLE = "Welcome to ProseFeed!"
+
 describe("Post Model", () => {
   it("should obtain the Welcome Post with resolvable fields", () => {
-    return new Post({title: "Welcome to ProseFeed!"}).fetch()
-    .then(post => {
+    return new Post({title: WELCOME_POST_TITLE}).fetch()
+    .then((post: Post) => {
       assert(post.id > 0)
-      assert.equal(post.title(), "Welcome to ProseFeed!")
+      assert.equal(post.title(), WELCOME_POST_TITLE)
+      assert(post.creationDate() > 0)
+      assert(post.updateDate() > 0)
+    })
+  })
+
+  it("should have a method to list posts", () => {
+    return Post.allPosts()
+    .then((posts: Array<Post>) => {
+      assert(posts.length > 0)
+      const post = posts.filter(p => p.title() === WELCOME_POST_TITLE)[0]
+      assert(post.id > 0)
+      assert.equal(post.title(), WELCOME_POST_TITLE)
       assert(post.creationDate() > 0)
       assert(post.updateDate() > 0)
     })
